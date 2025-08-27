469f475148fe3d4b9f0bc6faa2fc451cd186e579
davidpaarup <107766727+davidpaarup@users.noreply.github.com>
2025-08-27 23:13:26 +0200

    Using MSSQL

--- auth.ts
+++ auth.ts
@@ -1,8 +1,41 @@
 import { betterAuth } from "better-auth";
-import Database from "better-sqlite3";
+import { MssqlDialect } from "kysely";
+import * as Tedious from 'tedious'
+import * as Tarn from 'tarn'
  
+const dialect = new MssqlDialect({
+  tarn: {
+    ...Tarn,
+    options: {
+      min: 0,
+      max: 10,
+    },
+  },
+  tedious: {
+    ...Tedious,
+    connectionFactory: () => new Tedious.Connection({
+      authentication: {
+        options: {
+          password: process.env.DATABASE_PASSWORD,
+          userName: process.env.DATABASE_USERNAME,
+        },
+        type: 'default',
+      },
+      options: {
+        database: 'ia-prompt',
+        port: 1433,
+        trustServerCertificate: true,
+      },
+      server: 'ia-prompt.database.windows.net',
+    }),
+  },
+})
+
 export const auth = betterAuth({
-    database: new Database("./sqlite.db"),
+    database: {
+        dialect,
+        type: "mssql"
+    },
     socialProviders: {
         microsoft: {
             clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID as string,
