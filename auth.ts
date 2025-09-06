f27ec0f38704f19becafbacebcd66e91dcfb3767
davidpaarup <107766727+davidpaarup@users.noreply.github.com>
2025-09-06 19:25:01 +0200

    Referencing right variables

--- auth.ts
+++ auth.ts
@@ -50,8 +50,8 @@ export const auth = betterAuth({
             clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET as string
         },
         google: {
-            clientId: process.env.AUTH_GOOGLE_ID as string,
-            clientSecret: process.env.AUTH_GOOGLE_SECRET as string
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         }
     },
 })
